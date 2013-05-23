9db37abe4c684aaead5f515417ebb16a7a639585
WINNIE WU <weiwu@college.harvard.edu>
2013-05-23 11:43:09 -0400

    added logout button

--- gen/com/parse/starter/R.java
+++ gen/com/parse/starter/R.java
@@ -19,27 +19,28 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int class_recommendation=0x7f07000e;
-        public static final int clock=0x7f07000d;
+        public static final int class_recommendation=0x7f07000f;
+        public static final int clock=0x7f07000e;
         public static final int course_catalog=0x7f070008;
-        public static final int create_label=0x7f070010;
+        public static final int create_label=0x7f070011;
         public static final int help=0x7f07000a;
-        public static final int it_is_currently=0x7f07000c;
-        public static final int login_btn=0x7f070015;
+        public static final int it_is_currently=0x7f07000d;
+        public static final int login_btn=0x7f070016;
         public static final int login_email=0x7f070002;
         public static final int login_label=0x7f070001;
         public static final int login_pass=0x7f070003;
         public static final int loginsubmit=0x7f070004;
+        public static final int logout=0x7f07000b;
         public static final int my_courses=0x7f070009;
-        public static final int or_login_label=0x7f070014;
+        public static final int or_login_label=0x7f070015;
         public static final int or_register_label=0x7f070005;
-        public static final int random_class=0x7f07000f;
+        public static final int random_class=0x7f070010;
         public static final int register_btn=0x7f070006;
-        public static final int register_email=0x7f070011;
-        public static final int register_pass=0x7f070012;
-        public static final int registersubmit=0x7f070013;
+        public static final int register_email=0x7f070012;
+        public static final int register_pass=0x7f070013;
+        public static final int registersubmit=0x7f070014;
         public static final int shop_classes=0x7f070007;
-        public static final int time=0x7f07000b;
+        public static final int time=0x7f07000c;
         public static final int title=0x7f070000;
     }
     public static final class layout {
@@ -58,14 +59,15 @@ public final class R {
         public static final int hello=0x7f040000;
         public static final int help=0x7f04000e;
         public static final int login=0x7f040008;
+        public static final int logout=0x7f04000f;
         public static final int my_courses=0x7f04000d;
         public static final int or_login=0x7f040006;
         public static final int or_register=0x7f040009;
         public static final int pw=0x7f040004;
-        public static final int random_class=0x7f040010;
+        public static final int random_class=0x7f040011;
         public static final int registersubmit=0x7f040005;
         public static final int shop_classes=0x7f04000b;
-        public static final int time=0x7f04000f;
+        public static final int time=0x7f040010;
     }
     public static final class style {
         public static final int Button=0x7f050001;
--- src/com/parse/starter/Homescreen.java
+++ src/com/parse/starter/Homescreen.java
@@ -3,6 +3,8 @@ package com.parse.starter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DigitalClock;
 
 import com.parse.ParseAnalytics;
@@ -13,11 +15,23 @@ public class Homescreen extends Activity {
 		super.onCreate(savedInstanceState);
 		ParseUser currentUser = ParseUser.getCurrentUser();
 		if (currentUser == null) {
-	    	Intent intent = new Intent(Homescreen.this, Register.class);
-	    	startActivity(intent);
-		} 
+			Intent intent = new Intent(Homescreen.this, Login.class);
+			startActivity(intent);
+		}
 		setContentView(R.layout.main);
-		ParseAnalytics.trackAppOpened(getIntent());	
+		ParseAnalytics.trackAppOpened(getIntent());
 		DigitalClock dc = (DigitalClock) findViewById(R.id.clock);
+		Button logout = (Button) findViewById(R.id.logout);
+
+	}
+
+	public void logout(View view) {
+		ParseUser.logOut();
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser == null) {
+			Intent intent = new Intent(Homescreen.this, Login.class);
+			startActivity(intent);
+		}
+
 	}
 }
