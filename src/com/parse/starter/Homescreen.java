423cd965be26ffedce3342e6d87bf972aaa304d8
WINNIE WU <weiwu@college.harvard.edu>
2013-05-23 13:00:35 -0400

    added a bunch of blank pages to put stuff on

--- src/com/parse/starter/Homescreen.java
+++ src/com/parse/starter/Homescreen.java
@@ -25,13 +25,34 @@ public class Homescreen extends Activity {
 
 	}
 
-	public void logout(View view) {
-		ParseUser.logOut();
-		ParseUser currentUser = ParseUser.getCurrentUser();
-		if (currentUser == null) {
-			Intent intent = new Intent(Homescreen.this, Login.class);
-			startActivity(intent);
+	public void onClick(View view) {
+		switch (view.getId()) {
+
+		case R.id.shop_classes:
+			Intent intentShop = new Intent(Homescreen.this, ShopClasses.class);
+			startActivity(intentShop);
+			break;
+		case R.id.course_catalog:
+			Intent intentCatalog = new Intent(Homescreen.this, CourseCatalog.class);
+			startActivity(intentCatalog);
+			break;
+		case R.id.my_courses:
+			Intent intentMyCourses = new Intent(Homescreen.this, MyCourses.class);
+			startActivity(intentMyCourses);
+			break;
+		case R.id.help:
+			break;
+		case R.id.logout:
+			ParseUser.logOut();
+			ParseUser currentUser = ParseUser.getCurrentUser();
+			if (currentUser == null) {
+				Intent intentLogout = new Intent(Homescreen.this, Login.class);
+				startActivity(intentLogout);
+			}
+			break;
+
 		}
 
 	}
+
 }
