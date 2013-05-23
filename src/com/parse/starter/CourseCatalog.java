69e08c85bbb6917d359fd229171c1d6f6c2cc05f
WINNIE WU <weiwu@college.harvard.edu>
2013-05-23 13:15:28 -0400

    derp

---
+++ src/com/parse/starter/CourseCatalog.java
@@ -0,0 +1,21 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
+
+public class CourseCatalog extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser == null) {
+			Intent intent = new Intent(this, Login.class);
+			startActivity(intent);
+		}
+		setContentView(R.layout.catalog);
+		ParseAnalytics.trackAppOpened(getIntent());
+	}
+}
