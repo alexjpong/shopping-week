30fe515cd771454434fa1de688931beac5048b1f
Alex Pong <pong@college.harvard.edu>
2013-05-22 23:15:50 -0400

    re-adding

---
+++ src/com/parse/starter/Login.java
@@ -0,0 +1,46 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.LogInCallback;
+
+public class Login extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		ParseAnalytics.trackAppOpened(getIntent());		
+	}
+	public void loginSubmit(View v)
+	{
+    	EditText username = (EditText) findViewById(R.id.login_email);
+    	EditText password = (EditText) findViewById(R.id.login_pass);
+    	
+    	ParseUser.logInInBackground(username.getText().toString(),
+    			password.getText().toString(), new LogInCallback() {
+    		  public void done(ParseUser user, ParseException e) {
+    		    if (user != null) {
+    		      // Hooray! The user is logged in.
+    		    	Intent intent = new Intent(Login.this, Classes.class);
+			    	startActivity(intent);
+    		    } 
+    		    else {
+    		      // Login failed. Look at the ParseException to see what happened.
+					Log.e("login error", e.getMessage());
+    		    }
+    		  }
+    		});
+	}
+	public void gotoRegister(View v)
+	{
+    	Intent intent = new Intent(Login.this, Register.class);
+    	startActivity(intent);
+	}
+}
---
+++ src/com/parse/starter/Register.java
@@ -0,0 +1,66 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class Register extends Activity {
+	// testing hi
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+		ParseAnalytics.trackAppOpened(getIntent());		
+	}
+	public void registerSubmit(View v) {
+
+		EditText username = (EditText) findViewById(R.id.register_email);
+		EditText password = (EditText) findViewById(R.id.register_pass);
+
+		ParseUser user = new ParseUser();
+
+		user.setUsername(username.getText().toString());
+		user.setPassword(password.getText().toString());
+		user.setEmail(username.getText().toString());
+
+
+		// other fields can be set just like with ParseObject
+		//user.put("phone", "650-253-0000");
+		try
+		{
+			user.signUpInBackground(new SignUpCallback() {
+				public void done(ParseException e) {
+					if (e == null) {
+						// Hooray! Let them use the app now.
+						Intent intent = new Intent(Register.this, Classes.class);
+						startActivity(intent);
+					} 
+					else {
+						// Sign up didn't succeed. Look at the ParseException
+						// to figure out what went wrong
+						Log.e("register error", e.getMessage());
+					}
+				}
+			})
+			;
+		}
+		catch(Exception e)
+		{
+			Log.e("register error", e.getMessage());
+		}
+	}
+	public void gotoLogin(View v)
+	{
+		Intent intent = new Intent(Register.this, Login.class);
+		startActivity(intent);
+	}
+}
